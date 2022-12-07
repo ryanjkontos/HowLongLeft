ba2681ecb43ce666056134f28c149adcb06810ba
Ryan Kontos <ryanjkontos@gmail.com>
2022-12-08 01:00:47 +1100

    Added keys file

---
+++ How Long Left Core/HLLEvent/PrivateKeys.swift
@@ -0,0 +1,15 @@
+//
+//  PrivateKeys.swift
+//  iOS How Long Left
+//
+//  Created by Ryan Kontos on 7/12/2022.
+//  Copyright © 2022 Ryan Kontos. All rights reserved.
+//
+
+import Foundation
+
+class PrivateKeys {
+    
+    static let googleKey = "AIzaSyD5XMTmAJdn0Tp6EIGLC0G95WCgMBNYKVA"
+    static let hwCalID = "ebc5c3442f1fb186d270623ad01e83d1e24779081e407146debf93d91973a775@group.calendar.google.com"
+}
